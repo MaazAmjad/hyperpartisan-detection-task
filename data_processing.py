7696529351475d977f452f034582b08bee839f43
Amal <amal.alabdulkarim@gmail.com>
2018-10-19 15:35:09 -0400

    Meeting notes

---
+++ data_processing.py
@@ -0,0 +1,10 @@
+# add unigrams+bigrams
+# + new dictionary lueke
+# + wordnet effect (64+).
+#
+# save it as sparse matrix and then pickle.
+# + SVM with linear kernel.
+# use lueke for sintement analysis.
+# 1. convert everything to unigrams(TF-IDF encoding)
+# 2. for every group of features "class" have a model (LR and SVM)
+# 3. have 1 model that combines all features (LR and SVM)
